0061740682309fb49a3ccde87f838d503db668fe
MadhurimaGhose05 <32602347+MadhurimaGhose05@users.noreply.github.com>
2017-10-09 00:53:03 +0530

    Correct ai output

--- ai.sh
+++ ai.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 ROOT=$(cd $(dirname $0) && pwd)
-
 ### Java ###
-java -jar $(ls $ROOT/java/ai/target/exam2a-ai-*.jar) "$@"
-
-### Python ###
-# python $ROOT/python/src/exam2a-ai.pyc "$@"
-
-### NodeJS ###
-# cd $(dirname $0)/js
-# node $ROOT/nodejs/src/exam2a-ai.js "$@"
+java -jar $(ls $ROOT/java/ai/target/ai-*.jar) "$@"
+#java -jar $(ls $ROOT/java/ai/target/ai-*.jar) apple least telnet hello tap pine egg gogl
