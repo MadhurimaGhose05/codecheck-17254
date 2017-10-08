5327b55766ae1a653140ccfd01465253a62b09f2
MadhurimaGhose05 <32602347+MadhurimaGhose05@users.noreply.github.com>
2017-10-09 00:40:45 +0530

    Addition of word number and word length restriction function

---
+++ java/fw/src/main/java/codecheck/Referee.java
@@ -0,0 +1,82 @@
+/*
+    Player class of Simple Word Chain Framework for Shiritori
+    Create by Madhurima on 09/10/2017
+    Description: Referee class, become the referee of the game.
+    Because two players are needed, manage with Thread
+*/
+package codecheck;
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.BindException;
+import java.net.ServerSocket;
+
+import java.net.Socket;
+import  java.util.ArrayList ;
+
+public class Referee {
+
+    private static ServerSocket referee;
+    private static boolean isGameOver = false; 
+    private static ArrayList<Socket> players = new ArrayList<Socket>();
+    public static String word = null;
+    public static int activePlayerID = 0;
+
+    public Referee(int portNum) {
+        try {
+            //only two players can be connectable
+            referee = new ServerSocket(portNum, 2); 
+        } catch (BindException e) {
+            // e.printStackTrace();
+            System.exit(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void waitPlayer(){
+        try {
+            int playerID = 0;
+            while(true) {
+                Socket player = referee.accept();
+                players.add(player);
+                
+                new PlayerThread(player, players, playerID).start();
+
+                playerID++;
+            }
+        } catch (SocketException e) {
+            // e.printStackTrace();
+            System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    synchronized public static void showJudgementResult(String word, String playerStr) {
+        if(playerStr != null && !isGameOver) {
+            / / Ignore the words you sent before the two players get together.
+            if(word.equals(" ") || !Shiritori.dict.contains(word)) {
+                System.out.println(playerStr + " (NG):" + word);
+                System.out.println( "WIN - " + (playerStr.equals("FIRST") ? "SECOND" : "FIRST"));
+                isGameOver = true;
+            } else {
+                System.out.println(playerStr + " (OK): " + word);
+                Shiritori . Dict . Remove (Word);
+            }
+        }
+    }
+
+    synchronized public static boolean isGameOver() {
+        return isGameOver;
+    }
+
+    public static void gameOver() {
+        try {
+            referee.close();
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println("Errors occured when closing Socket or ServerSocket!");
+            e.printStackTrace();
+        }
+    }
+}
