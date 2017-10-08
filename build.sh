63a24941f64c4683af3ea1f1aa76c432e993291b
MadhurimaGhose05 <32602347+MadhurimaGhose05@users.noreply.github.com>
2017-10-09 01:02:17 +0530

    Correct ai output

--- build.sh
+++ build.sh
@@ -5,10 +5,8 @@ ROOT=$(cd $(dirname $0) && pwd)
 ### Java ###
 cd $ROOT/java/fw
 mvn package -Dmaven.test.skip=true
+
+mvn install:install-file -Dfile=$ROOT/java/fw/target/fw-1.0-SNAPSHOT.jar -DgroupId=codecheck -DartifactId=fw -Dversion=1.0-SNAPSHOT -Dpackaging=jar
+
 cd $ROOT/java/ai
 mvn package -Dmaven.test.skip=true
-
-### Python ###
-# cd $ROOT/python/src
-# python -m compileall .
-
