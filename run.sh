ae72e04fdda30c4001d040a4a61eeb7deea02292
MadhurimaGhose05 <32602347+MadhurimaGhose05@users.noreply.github.com>
2017-10-09 01:15:39 +0530

    Editing a production file

--- run.sh
+++ run.sh
@@ -2,12 +2,19 @@
 
 ROOT=$(cd $(dirname $0) && pwd)
 
+for  i  in  " $ @ "
+do
+	case  $ i  in
+	" ./ai.sh " ) ;;
+	* ) dict=$dict" "$i ;;
+	esac
+done
+
 ### Java ###
-java -jar $(ls $ROOT/java/fw/target/exam2a-framework-*.jar) "$@"
+java -jar $(ls $ROOT/java/fw/target/fw-*.jar) $dict &
 
-### Python ###
-# python $ROOT/python/src/exam2a-fw.pyc "$@"
+sleep 1
 
-### NodeJS ###
-# cd $(dirname $0)/js
-# node $ROOT/nodejs/src/exam2a-fw.js "$@"
+./ai.sh $ dict  &
+
+./ai.sh $ dict
